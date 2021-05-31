aab5143737059f6811d6e417ae8782aa6fe558f5
Maciej <maciejgrochowski93@o2.pl>
2021-06-01 01:25:22 +0200

    adding Jasypt encoding, updating configuration endpoints

--- src/main/java/maciej/grochowski/grocerystore/GroceryStoreApplication.java
+++ src/main/java/maciej/grochowski/grocerystore/GroceryStoreApplication.java
@@ -3,13 +3,15 @@ package maciej.grochowski.grocerystore;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 @EnableEncryptableProperties
 public class GroceryStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GroceryStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryStoreApplication.class, args);
+    }
 
 }
